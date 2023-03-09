704d405e10ab700c29e601379d300227de1e8b54
Enzo Innocenzi <enzo@innocenzi.dev>
2023-03-09 12:52:58 +0100

    feat(tables): support disabling endpoint

--- packages/laravel/routes/hybridly.php
+++ packages/laravel/routes/hybridly.php
@@ -3,5 +3,7 @@
 use Hybridly\Tables\Http\Controllers\HybridEndpointController;
 use Illuminate\Support\Facades\Route;
 
-Route::post(config('hybridly.endpoint_path', 'hybridly'), HybridEndpointController::class)
-    ->name('hybridly.endpoint');
+if (config('hybridly.endpoint_enabled', true)) {
+    Route::post(config('hybridly.endpoint_path', 'hybridly'), HybridEndpointController::class)
+        ->name('hybridly.endpoint');
+}

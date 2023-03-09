21dfcc2c1a84f3d83a4fd2d76b6cfb1493acf95e
Enzo Innocenzi <enzo@innocenzi.dev>
2023-03-09 12:56:13 +0100

    test: update route extractor test

--- tests/src/Laravel/RouteExtractor/RouteExtractorTest.php
+++ tests/src/Laravel/RouteExtractor/RouteExtractorTest.php
@@ -9,9 +9,37 @@ beforeEach(function () {
     app()->setBasePath(str_replace('/vendor/orchestra/testbench-core/laravel', '', app()->basePath()));
 });
 
+test('built-in routes are extracted', function () {
+    expect(app(RouteExtractor::class)->getRoutes())
+        ->toHaveCount(1)
+        ->sequence(
+            fn (Expectation $route) => $route->toBe([
+                'domain' => null,
+                'method' => ['POST'],
+                'uri' => 'hybridly',
+                'name' => 'hybridly.endpoint',
+                'bindings' => [],
+                'wheres' => [],
+            ]),
+        );
+});
+
+test('routes can be filtered out', function () {
+    Route::get('/filtered-route', fn () => response())->name('filtered-route');
+    Route::get('/ok', fn () => response())->name('ok');
+
+    config(['hybridly.router.exclude' => ['hybridly', 'filtered*']]);
+
+    expect(app(RouteExtractor::class)->getRoutes())
+        ->toHaveCount(1)
+        ->sequence(fn ($_, $key) => $key->toBe('ok'));
+});
+
 test('only named routes are extracted', function () {
     Route::get('/not-named', fn () => response());
     Route::get('/named', fn () => response())->name('named');
+
+    config(['hybridly.router.exclude' => 'hybridly']);
 
     expect(app(RouteExtractor::class)->getRoutes())
         ->toHaveCount(1)
@@ -27,19 +55,10 @@ test('only named routes are extracted', function () {
         );
 });
 
-test('routes can be filtered out', function () {
-    Route::get('/filtered-route', fn () => response())->name('filtered-route');
-    Route::get('/ok', fn () => response())->name('ok');
-
-    config(['hybridly.router.exclude' => 'filtered*']);
-
-    expect(app(RouteExtractor::class)->getRoutes())
-        ->toHaveCount(1)
-        ->sequence(fn ($_, $key) => $key->toBe('ok'));
-});
-
 test('routes from vendors are excluded by default', function () {
     Route::get('/vendor-route', [BroadcastController::class, 'authenticate'])->name('vendor-route');
+
+    config(['hybridly.router.exclude' => 'hybridly']);
 
     expect(app(RouteExtractor::class)->getRoutes())->toHaveCount(0);
 });
@@ -48,6 +67,7 @@ test('routes from vendors can be opted-in', function () {
     Route::get('/vendor-route', [BroadcastController::class, 'authenticate'])->name('vendor-route');
 
     config(['hybridly.router.allowed_vendors' => ['laravel/framework']]);
+    config(['hybridly.router.exclude' => 'hybridly']);
 
     expect(app(RouteExtractor::class)->getRoutes())
         ->toHaveCount(1)
